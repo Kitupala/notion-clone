6df192d46667f72ab5b6bc2859b3e3b1b35ec0fc
Kitupala <kimmo.tuupanen@gmail.com>
2024-12-18 19:40:46 +0200

    remove auth check

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -4,22 +4,13 @@ import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { createNewDocument } from "@/actions/actions";
-import { useUser } from "@clerk/nextjs";
 
 const NewDocumentButton = () => {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
-  const { user, isLoaded } = useUser();
 
   const handleCreateNewDocument = () => {
     startTransition(async () => {
-      if (!user && isLoaded) {
-        // Redirect the user to the sign-in page
-        router.push("/sign-in");
-        return;
-      }
-
-      // Create a new document
       const { docId } = await createNewDocument();
       router.push(`/doc/${docId}`);
     });
